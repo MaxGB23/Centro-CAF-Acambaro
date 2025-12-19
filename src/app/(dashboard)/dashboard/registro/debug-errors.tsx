41d799af09c19e06e88f016f40650ebc7a4e06d4
MaxGB23 <cb147.gonzalez.ballesteros.max@gmail.com>
2025-12-18 19:25:47 -0600

    chore: remove unnecessary files

--- src/app/(dashboard)/dashboard/registro/debug-errors.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { createAuthClient } from "better-auth/client";
-
-export function DebugErrors() {
-  const authClient = createAuthClient();
-  console.log(authClient.$ERROR_CODES);
-  return null;
-}
